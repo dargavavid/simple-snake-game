71ef4f0f22b9a53e4a136c1e295ce664f7ae55ee
dargavavid <dargavavid@gmail.com>
2017-12-21 21:21:21 +0100

    Add base snake class, test it

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+//Create snake class:
+class Snake {
+    constructor(x1 = 0, y1 = 0, dx = 0, dy = 1) {
+        this.body = [{x: x1, y: y1}];
+        this.dx = dx;
+        this.dy = dy;
+    }
+}
+
 //Create 2D grid with n rows and m columns:
 function makeGrid(n, m) {
     return new Array(n).fill(0).map(row => new Array(m).fill(0));
@@ -5,6 +14,7 @@ function makeGrid(n, m) {
 
 const app = {
     canvas: document.querySelector("#canvas"),
-    canvasCtx: this.canvas.getContext("2d")
+    canvasCtx: this.canvas.getContext("2d"),
+    snake: new Snake(0, 0, 0, 1),
 };
 
--- js/mainSpec.js
+++ js/mainSpec.js
@@ -1,8 +1,16 @@
 const T = new Tester();
 
-T.test("Should create 2D array", function() {
+T.test("Should create 2D array.", function() {
     const testArr = [[0,0], [0,0]];
     const testArr2 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     T.assertSimilar(makeGrid(2, 2), testArr);
     T.assertSimilar(makeGrid(3, 3), testArr2);
+});
+
+T.test("Should create a snake object with specified parameters.", function() {
+    const snake = new Snake(10, 11, 5, 7);
+    T.assertEquals(snake.body[0].x, 10);
+    T.assertEquals(snake.body[0].x, 10);
+    T.assertEquals(snake.dx, 5);
+    T.assertEquals(snake.dy, 7);
 });
